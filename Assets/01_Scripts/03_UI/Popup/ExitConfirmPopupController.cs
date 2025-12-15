3500aebe8eeafee004db8341fa0c116b433dcda6
rwy685 <rwy685@gmail.com>
2025-12-15 20:27:04 +0900

    feat.Mainmenu UI_게임종료팝업기능 추가 및 타이틀폰트/이미지 추가

---
+++ Assets/01_Scripts/03_UI/Popup/ExitConfirmPopupController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitConfirmPopupController : MonoBehaviour
+{
+    [SerializeField] private Button btnYes;
+    [SerializeField] private Button btnNo;
+
+    private PopupCanvasRootController root;
+
+    private void Awake()
+    {
+        root = GetComponentInParent<PopupCanvasRootController>();
+
+        btnYes.onClick.AddListener(OnClickYes);
+        btnNo.onClick.AddListener(OnClickNo);
+
+        gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnClickYes()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    private void OnClickNo()
+    {
+        root.CloseAllPopups();
+    }
+}
---
+++ Assets/01_Scripts/03_UI/Popup/PopupCanvasRootContorller.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PopupCanvasRootController : MonoBehaviour
+{
+    [SerializeField] private ExitConfirmPopupController exitConfirmPopup;
+
+    private System.Action<ShowExitConfirmPopupEvent> showExitHandler;
+
+    private void Awake()
+    {
+        showExitHandler = OnShowExitConfirm;
+
+        exitConfirmPopup.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        EventBus.Subscribe(showExitHandler);
+    }
+
+    private void OnDisable()
+    {
+        EventBus.Unsubscribe(showExitHandler);
+    }
+
+    private void OnShowExitConfirm(ShowExitConfirmPopupEvent evt)
+    {
+        Debug.Log("ExitConfirmPopupEvent received");
+        exitConfirmPopup.Show();
+    }
+
+    public void CloseAllPopups()
+    {
+        exitConfirmPopup.Hide();
+    }
+}
+
