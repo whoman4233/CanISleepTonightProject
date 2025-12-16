664ac51353dcb8a85b2366b5abe7c9011a36bc1c
rwy685 <rwy685@gmail.com>
2025-12-16 15:38:29 +0900

    feat.InGameMenu UI 추가/UIStateManager 추가 및 이벤트 추가

--- Assets/01_Scripts/03_UI/EventStruct.cs
+++ Assets/01_Scripts/03_UI/EventStruct.cs
@@ -1,42 +1,86 @@
-﻿public struct GameTimeUpdateEvent
+﻿
+//==========================================
+//GamePhase관련 이벤트 목록
+//==========================================
+
+public struct GamePhaseChangedEvent //게임 페이즈에 따른 UI 변경
+{
+    public GamePhase Phase;
+
+    public GamePhaseChangedEvent(GamePhase phase)
+    {
+        Phase = phase;
+    }
+}
+
+//==========================================
+//게임 신 관련 이벤트 목록
+//==========================================
+
+public struct GameTimeUpdateEvent //타이머
 {
     public float Seconds;
 }
-//=====================
+
+//==========================================
 //MainMenuUI 이벤트 목록
-//=====================
+//==========================================
 
-public struct ShowMainMenuEvent
+public struct ShowMainMenuEvent //메인 메뉴 노출
 {
 
 }
 
-public struct HideMainMenuEvent
+public struct HideMainMenuEvent //메인 메뉴 숨기기
 {
 
 }
 
-public struct ShowExitConfirmPopupEvent
+
+
+public struct StartNewGameEvent // 새 게임 시작
 {
 
 }
 
-public struct StartNewGameEvent
+public struct LoadGameEvent // 이어하기
 {
 
 }
 
-public struct LoadGameEvent
+//==========================================
+//PopupUI 이벤트 목록
+//==========================================
+
+public struct ShowExitConfirmPopupEvent //게임 종료 팝업 노출
+{
+
+}
+public struct ShowSettingsPopupEvent // 옵션팝업 노출
 {
 
 }
 
-public struct ShowSettingsPopupEvent
+public struct HideSettingsPopupEvent //옵션 팝업 숨기기
 {
 
 }
 
-public struct HideSettingsPopupEvent
+//==========================================
+//InGameMenu 이벤트 목록
+//==========================================
+
+public struct PauseMenuToggleRequestedEvent // 게임메뉴 켜졌을 때 게임매니저 참고용 이벤트(Toggle)
+{
+
+}
+
+public struct ResumeGameRequestedEvent //게임 재개
+{
+
+}
+
+public struct ReturnToTitleRequestedEvent //타이틀로 돌아가기
 {
 
 }
