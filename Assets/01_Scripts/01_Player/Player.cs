426e937eeb04aa99080df2875d326895d83df439
hyeonwooJ <janghw4116@gmail.com>
2025-12-19 21:19:44 +0900

    fix: 삭제된 데이터 복구
    삭제된 데이터 복구

--- Assets/01_Scripts/01_Player/Player.cs
+++ Assets/01_Scripts/01_Player/Player.cs
@@ -9,12 +9,12 @@ public class Player : MonoBehaviour
 
     [Header("Refs (Cache)")]
     [SerializeField] private PlayerWeaponHandler weaponHandler;
+
     public Animator Animator { get; private set; }
     public CharacterController Controller { get; private set; }
     public ForceReceiver ForceReceiver { get; private set; }
     public bool Interaction { get; private set; }
     public PlayerStateMachine StateMachine { get; private set; }
-    public Transform MainCameraTransform { get; private set; }
 
     // PlayerInputs 기반 입력 캐시 (FSM이 읽어감)
     public Vector2 MoveInput { get; private set; }
@@ -32,18 +32,12 @@ public class Player : MonoBehaviour
     public PlayerInputs Inputs => _inputs;
 
     private bool _isInspectionLocked; // Inspection(상세보기) 중 플레이어 입력 차단 플래그
-
     private InspectionManager _inspectionManager;
     private void Awake()
     {
         Animator = GetComponentInChildren<Animator>();
         Controller = GetComponent<CharacterController>();
         ForceReceiver = GetComponent<ForceReceiver>();
-
-        if (weaponHandler == null)
-            weaponHandler = GetComponent<PlayerWeaponHandler>();
-
-        MainCameraTransform = Camera.main != null ? Camera.main.transform : null;
 
         if (AnimationData == null)
         {
@@ -53,8 +47,10 @@ public class Player : MonoBehaviour
         }
 
         AnimationData.Initialize();
+
         _inputs = new PlayerInputs();
         _playerActions = _inputs.Player;
+
         StateMachine = new PlayerStateMachine(this);
 
         _inspectionManager = GetComponentInChildren<InspectionManager>();
@@ -67,7 +63,6 @@ public class Player : MonoBehaviour
             _inspectionManager.Initialize(_inputs);
         }
     }
-
     private void OnEnable()
     {
         _inputs.Enable();
@@ -75,7 +70,7 @@ public class Player : MonoBehaviour
         _inputs.Inspection.Disable();
 
         //상세보기 진입 구독
-        EventBus.Subscribe<InspectionStartedEvent>(OnInspectionStarted); 
+        EventBus.Subscribe<InspectionStartedEvent>(OnInspectionStarted);
         EventBus.Subscribe<InspectionEndedEvent>(OnInspectionEnded);
     }
     private void OnDisable()
@@ -103,6 +98,7 @@ public class Player : MonoBehaviour
         StateMachine.ChangeState(StateMachine.Locomotion);
     }
     public PlayerWeaponHandler WeaponHandler => weaponHandler;
+
     private void Update()
     {
         if (_isInspectionLocked) // 상세보기 진입시 플레이어 입력 잠그기
@@ -144,6 +140,7 @@ public class Player : MonoBehaviour
         _isInspectionLocked = false;
         _inputs.Player.Enable();
     }
+
     public void TryEnterInspection(IInspectable inspectable)
     {
         if (_inspectionManager == null)
