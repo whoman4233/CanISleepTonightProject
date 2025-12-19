793f1d120661f8db63d10f3840b19d86f4ac9c39
rwy685 <rwy685@gmail.com>
2025-12-19 17:40:40 +0900

    test.UIScene

--- Assets/01_Scripts/03_UI/Mainmenu/MainMenuRootController.cs
+++ Assets/01_Scripts/03_UI/Mainmenu/MainMenuRootController.cs
@@ -1,42 +1,30 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MainMenuRootController : MonoBehaviour
 {
     [SerializeField] private GameObject menuRoot;
 
-    private Action<GamePhaseChangedEvent> _handler;
-
     private void Awake()
     {
-        _handler = OnPhaseChanged;
-        menuRoot.SetActive(false);
+        if (menuRoot != null)
+            menuRoot.SetActive(false);
     }
 
     private void OnEnable()
     {
-        EventBus.Subscribe(_handler);
-
-        // 현재 Phase 즉시 반영 (타이밍 문제 해결)
-        var gm = GameContext.Instance.Get<GameManager>();
-        ApplyPhase(gm.CurrentPhase);
+        EventBus.Subscribe<GamePhaseChangedEvent>(OnPhaseChanged);
     }
 
     private void OnDisable()
     {
-        EventBus.Unsubscribe(_handler);
+        EventBus.Unsubscribe<GamePhaseChangedEvent>(OnPhaseChanged);
     }
 
     private void OnPhaseChanged(GamePhaseChangedEvent e)
     {
-        ApplyPhase(e.Phase);
-    }
+        bool isMenuPhase = e.Phase == GamePhase.NotStarted;
 
-    private void ApplyPhase(GamePhase phase)
-    {
-        menuRoot.SetActive(phase == GamePhase.NotStarted);
+        if (menuRoot != null)
+            menuRoot.SetActive(isMenuPhase);
     }
 }
-
-
-
