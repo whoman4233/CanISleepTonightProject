f9dca87d3398a577e932d3833807a80958e0b2c4
rwy685 <rwy685@gmail.com>
2025-12-22 15:15:45 +0900

    fix.상세보기 관련 스크립트 수정

---
+++ Assets/01_Scripts/03_UI/Inspection/InspectableObject/HiddenItemWorldVisual.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class HiddenItemWorldVisual : MonoBehaviour
+{
+    [SerializeField] private HiddenItemHolder itemHolder;
+    [SerializeField] private HiddenItemStateSO targetItem;
+    [SerializeField] private GameObject worldVisual;
+
+    private void Start()
+    {
+        if (itemHolder == null || targetItem == null)
+            return;
+
+        targetItem.OnFoundStateChanged += OnFoundChanged;
+        OnFoundChanged(targetItem.IsFound);
+    }
+
+    private void OnDestroy()
+    {
+        if (targetItem != null)
+            targetItem.OnFoundStateChanged -= OnFoundChanged;
+    }
+
+    private void OnFoundChanged(bool isFound)
+    {
+        if (worldVisual != null)
+            worldVisual.SetActive(!isFound);
+    }
+}
---
+++ Assets/01_Scripts/03_UI/Inspection/InspectableObject/InspectableObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class InspectableObject : MonoBehaviour, IInteractable, IInspectable
+{
+    [Header("Inspection")]
+    [SerializeField] private GameObject inspectPrefab;
+    [SerializeField] private GameObject visualRoot;
+
+    public Transform GetInspectPivot() => transform;
+    public GameObject GetInspectPrefab() => inspectPrefab;
+
+    public void OnInspectionEnter()
+    {
+        if (visualRoot != null)
+            visualRoot.SetActive(false);
+    }
+
+    public void OnInspectionExit()
+    {
+        if (visualRoot != null)
+            visualRoot.SetActive(true);
+    }
+
+    public void Interact(Player player)
+    {
+        player.TryEnterInspection(this);
+    }
+}
