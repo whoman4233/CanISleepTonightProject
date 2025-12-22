c2cefc49f9d6cb35e421545a853cd16c8c9ad552
rwy685 <rwy685@gmail.com>
2025-12-22 20:05:03 +0900

    feat.UIInput 추가 및 GameManager 페이즈 이벤트할당

---
+++ Assets/01_Scripts/03_UI/UIInputPhaseController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UIInputPhaseController : MonoBehaviour
+{
+    [SerializeField]
+    private InputActionAsset uiActions; // EventSystem에 연결된 것과 동일한 에셋
+
+    private void OnEnable()
+    {
+        EventBus.Subscribe<GamePhaseChangedEvent>(OnPhaseChanged);
+    }
+
+    private void OnDisable()
+    {
+        EventBus.Unsubscribe<GamePhaseChangedEvent>(OnPhaseChanged);
+    }
+
+    private void OnPhaseChanged(GamePhaseChangedEvent e)
+    {
+        if (e.Phase == GamePhase.NotStarted)
+        {
+            uiActions.FindActionMap("UI", true).Enable();
+
+            var playerMap = uiActions.FindActionMap("Player", false);
+            if (playerMap != null) playerMap.Disable();
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (GameManager.Instance.CurrentPhase == GamePhase.NotStarted)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
