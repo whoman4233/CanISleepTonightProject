54608d554a6729a01df583587edb0ad8627c3352
rwy685 <rwy685@gmail.com>
2025-12-23 20:46:56 +0900

    feat.Settlement트리거/경고팝업UI

---
+++ Assets/01_Scripts/03_UI/Result/SettlementTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SettlementTrigger : MonoBehaviour, IInteractable
+{
+    public bool CanInteract()
+    {
+        return GameManager.Instance.CurrentPhase == GamePhase.Patrol;
+    }
+
+    public void Interact(Player player)
+    {
+        if (!CanInteract())
+        {
+            EventBus.Publish(new ShowWarningPopupEvent());
+            return;
+        }
+
+        EventBus.Publish(new RequestPhaseChangeEvent(GamePhase.Settlement));
+    }
+}
