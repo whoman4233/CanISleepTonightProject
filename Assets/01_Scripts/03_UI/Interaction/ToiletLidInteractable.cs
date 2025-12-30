71985aac3d450ba3ca887dc28071fad2884a223f
rwy685 <rwy685@gmail.com>
2025-12-30 21:16:22 +0900

    feat.변기 상호작용

---
+++ Assets/01_Scripts/03_UI/Interaction/ToiletLidInteractable.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class ToiletLidInteractable : MonoBehaviour, IInteractable
+{
+    [Header("Animator")]
+    [SerializeField] private Animator animator;
+
+    [Header("Animator Params")]
+    [SerializeField] private string openTriggerName = "Open";
+    [SerializeField] private string closeTriggerName = "Close";
+
+    private int _openTriggerHash;
+    private int _closeTriggerHash;
+
+    private bool _isOpen;
+
+    private void Awake()
+    {
+        if (animator == null)
+            animator = GetComponentInChildren<Animator>();
+
+        _openTriggerHash = Animator.StringToHash(openTriggerName);
+        _closeTriggerHash = Animator.StringToHash(closeTriggerName);
+    }
+
+    public void Interact(Player player)
+    {
+        ToggleLid();
+    }
+
+    private void ToggleLid()
+    {
+        if (_isOpen)
+        {
+            animator.SetTrigger(_closeTriggerHash);
+        }
+        else
+        {
+            animator.SetTrigger(_openTriggerHash);
+        }
+
+        _isOpen = !_isOpen;
+    }
+}
