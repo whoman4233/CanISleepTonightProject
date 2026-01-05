c7ad041b15e1682b487f2e84e5e904bf705f3cfe
Lim <mlimsg08@gmail.com>
2026-01-05 15:41:04 +0900

    feat: input enum에 대화상태 추가

--- Assets/01_Scripts/05_Manager/Enum/InputMode.cs
+++ Assets/01_Scripts/05_Manager/Enum/InputMode.cs
@@ -2,5 +2,6 @@
 {
     Gameplay,      // 일반 플레이
     UIOnly,        // 메뉴 / 인트로 / UI 상호작용
-    Inspection     // Inspection 전용
+    Inspection,     // Inspection 전용
+    Dialogue // 대화전용(대화 중 다른 상호작용 금지)
 }
