5375437e390225b6e991bc321ba196ae477e2e74
Lim <mlimsg08@gmail.com>
2026-01-13 12:31:52 +0900

    fix: 죄수 대화 로직 변경

--- Assets/01_Scripts/09_Dialogue/NPC/PrisonerDialogue.cs
+++ Assets/01_Scripts/09_Dialogue/NPC/PrisonerDialogue.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PrisonerDialogue : MonoBehaviour , IInteractable 
+public class PrisonerDialogue : MonoBehaviour, IInteractable
 {
     public VisualAnomalyType myVisualType;
     private string _mySpeakerKey = null; // 기본값은 null
@@ -14,36 +14,41 @@ public class PrisonerDialogue : MonoBehaviour , IInteractable
         {
             dialogueManager = GameObject.FindAnyObjectByType<DialogueManager>();
         }
-        InitializeIdentity();
+        //InitializeIdentity();
     }
-    public void InitializeIdentity()
-    {
-        // 용의자들만 대사 키를 할당받음
-        switch (myVisualType)
-        {
-            case VisualAnomalyType.Suspect1:
-                _mySpeakerKey = DialogueKeys.Speakers.Suspect1;
-                break;
-            case VisualAnomalyType.Suspect2:
-                _mySpeakerKey = DialogueKeys.Speakers.Suspect2;
-                break;
-            case VisualAnomalyType.Suspect3:
-                _mySpeakerKey = DialogueKeys.Speakers.Suspect3;
-                break;
-            default:
-                _mySpeakerKey = null; // 그 외 일반 죄수는 키 없음
-                break;
-        }
-    }
+    //public void InitializeIdentity()
+    //{
+    //    // 용의자들만 대사 키를 할당받음
+    //    switch (myVisualType)
+    //    {
+    //        case VisualAnomalyType.Suspect1:
+    //            _mySpeakerKey = DialogueKeys.Speakers.Suspect1;
+    //            break;
+    //        case VisualAnomalyType.Suspect2:
+    //            _mySpeakerKey = DialogueKeys.Speakers.Suspect2;
+    //            break;
+    //        case VisualAnomalyType.Suspect3:
+    //            _mySpeakerKey = DialogueKeys.Speakers.Suspect3;
+    //            break;
+    //        default:
+    //            _mySpeakerKey = null; // 그 외 일반 죄수는 키 없음
+    //            break;
+    //    }
+    //}
+
 
     public void Interact(Player player)
     {
-        if (string.IsNullOrEmpty(_mySpeakerKey))
+        if (dialogueManager == null) return;
+
+        string speakerKey = myVisualType.ToString();
+
+        if (myVisualType == VisualAnomalyType.None)
         {
-            Debug.Log($"{gameObject.name}: 대사 데이터가 없는 일반 죄수입니다.");
+            Debug.Log($"{gameObject.name}: 일반 죄수는 대사 데이터가 없습니다.");
             return;
         }
-
-        dialogueManager.StartDialogueByKeys(_mySpeakerKey);
+        dialogueManager.StartDialogueByKeys(speakerKey, DialogueKeys.Types.Dialogue);
     }
 }
+
