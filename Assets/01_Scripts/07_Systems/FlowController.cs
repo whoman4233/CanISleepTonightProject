9644414697a7a77496c1f10bf27bf3f27673eb9c
Lim <mlimsg08@gmail.com>
2026-01-30 19:49:02 +0900

    fix: 재로딩 추가 수정

--- Assets/01_Scripts/07_Systems/FlowController.cs
+++ Assets/01_Scripts/07_Systems/FlowController.cs
@@ -218,7 +218,7 @@ public class FlowController : MonoBehaviour
             }
         }
         GameManager.Instance.ChangePhase(GamePhase.Standby); // 페이즈 전환
-        yield return new WaitForSecondsRealtime(0.1f);
+        //yield return new WaitForSecondsRealtime(0.1f);
         IsBusy = false;
         //yield return new WaitForSeconds(0.5f); // 추가로 0.5초 로딩화면 보여줌 추후 브리핑 페이즈에 로딩씬 끝나게?
         yield return SceneManager.UnloadSceneAsync(loadingSceneName); // 로딩 씬 언로드
