78849cadafd9fd09657aabb4d27c4173053112e9
rwy685 <rwy685@gmail.com>
2026-02-13 14:14:25 +0900

    fix.대사 언어변경 감지시 실시간 변화 적용

--- Assets/01_Scripts/03_UI/Language/LocalizedText.cs
+++ Assets/01_Scripts/03_UI/Language/LocalizedText.cs
@@ -1,13 +1,16 @@
 ﻿using TMPro;
 using UnityEngine;
+using System;
 
 [RequireComponent(typeof(TMP_Text))]
 public class LocalizedText : MonoBehaviour
 {
     [SerializeField] private string textId;
-    [SerializeField] private TextTableType tableType;
+    [SerializeField] private TextTableType tableType = TextTableType.UI;
 
     private TMP_Text _text;
+    private string _runtimeOverrideId; // 런타임용 ID
+    private bool _useRuntimeId;
 
     private void Awake()
     {
@@ -27,29 +30,42 @@ public class LocalizedText : MonoBehaviour
 
     public void SetRuntimeId(string id)
     {
-        textId = id;
+        _runtimeOverrideId = id;
+        _useRuntimeId = true;
+        Refresh();
+    }
+
+    public void ClearRuntimeOverride()
+    {
+        _useRuntimeId = false;
         Refresh();
     }
 
     private void Refresh()
     {
-        if (string.IsNullOrEmpty(textId))
+        if (TextManager.Instance == null)
+            return;
+
+        string id = _useRuntimeId ? _runtimeOverrideId : textId;
+
+        if (string.IsNullOrEmpty(id))
             return;
 
         switch (tableType)
         {
             case TextTableType.Dialogue:
-                _text.text = TextManager.Instance.GetText(textId);
+                _text.text = TextManager.Instance.GetText(id);
                 break;
 
             case TextTableType.UI:
-                _text.text = TextManager.Instance.GetUIText(textId);
+                _text.text = TextManager.Instance.GetUIText(id);
                 break;
 
             case TextTableType.Prompt:
-                _text.text = TextManager.Instance.GetPromptText(textId);
+                _text.text = TextManager.Instance.GetPromptText(id);
                 break;
         }
     }
 }
+
 
