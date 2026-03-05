97a99b00a0e6d84d291ccebf6ab1caa303e8ec1b
Lim <mlimsg08@gmail.com>
2026-03-05 21:51:13 +0900

    chore: 20퍼경직

--- Assets/01_Scripts/02_Prisoner/AI/States/PrisonerCombatState.cs
+++ Assets/01_Scripts/02_Prisoner/AI/States/PrisonerCombatState.cs
@@ -212,7 +212,7 @@ public class PrisonerCombatState : BasePrisonerState
         //anim.SetTrigger(HitTriggerHash);
         //_isAttackStarted = false;
         //ForceStopPhysicalMovement();
-        if (Random.value <= 0.8f)
+        if (Random.value <= 0.2f)
         {
             anim.SetTrigger(HitTriggerHash);
 
